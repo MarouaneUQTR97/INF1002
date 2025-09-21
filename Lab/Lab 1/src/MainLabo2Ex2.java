338812edb70a0550f4adcdc310407d7dd9839859
MarouaneUQTR97 <marouane.berrami@uqtr.ca>
2025-09-21 15:40:13 -0400

    Delete Lab directory

--- Lab/Lab 1/src/MainLabo2Ex2.java
+++
@@ -1,5 +0,0 @@
-public class MainLabo2Ex2 {
-    public static void main(String[] args) {
-        System.out.print("Ce cours d'"+"introdutcion a la programmation"+" de l'automne "+(2000+17)+" est génial, je lui donne la note de A+");
-    }
-}
