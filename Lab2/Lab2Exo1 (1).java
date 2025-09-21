8c3f216c1cd9e2922746b023a1e5729dfa269e2a
MarouaneUQTR97 <Marouane.berrami@uqtr.ca>
2025-09-21 17:53:19 -0400

    Push Lab2

---
+++ Lab2/Lab2Exo1 (1).java
@@ -0,0 +1,29 @@
+/*
+ * Description : classe permettant l'affichage d'une liste de deux étudiants
+ * Auteur : Catherine Béliveau, Mourad Badri, Louis Hamel
+ */
+package src;
+
+public class Main {
+	/**
+	 * Méthode pilote permettant l'affichage de la liste
+	 * 
+	 * @param args Arguments du programme. Aucun n'est requis pour celui-ci.
+	 */
+	public static void main(String[] args) {
+
+		/*
+		 * Les prochaîne lignes affichent une liste.
+		 */
+		System.out.print("\r");
+		System.out.print("Liste d'élèves :\r");
+		System.out.print("\r");
+		System.out.print("==================================================\r");
+		System.out.print("\tNom\t\tPrénom\r"); // entête
+		System.out.print("\t====\t\t========\r");
+		System.out.print("\tBéliveau\tCatherine\r"); // étudiants
+		System.out.print("\tDesjardins\tMathieu\r");
+		System.out.print("===================================================\r");
+	}
+
+}
